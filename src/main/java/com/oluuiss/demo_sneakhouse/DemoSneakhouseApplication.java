609547b44db9041194d70a646cdf8f28153cf8e4
Luis <luispyim@gmail.com>
2025-06-17 23:02:32 -0300

    terminando a estilização na tela de login

---
+++ src/main/java/com/oluuiss/demo_sneakhouse/DemoSneakhouseApplication.java
@@ -0,0 +1,13 @@
+package com.oluuiss.demo_sneakhouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSneakhouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSneakhouseApplication.class, args);
+	}
+
+}
